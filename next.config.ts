db40dd6d467d30fabaa39724966fb02c52baa28e
MiguelAdrian <132788574+MiguelAdrian138@users.noreply.github.com>
2024-12-08 12:11:05 -0500

    ignorar eslint

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  ppr:'incremental'
+  ppr: "incremental",
+  eslint: {
+    // disable during development and build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
